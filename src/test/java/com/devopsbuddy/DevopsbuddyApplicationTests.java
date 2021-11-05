79b740c23327ef23f25af7d706e19a0509a02f23
RogerioOcchiuzzi <rogerio.occhiuzzi@gmail.com>
2021-11-05 20:52:55 -0300

    Adicionou I18N no site

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,13 +1,25 @@
 package com.devopsbuddy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import com.devopsbuddy.web.i18n.I18NService;
 
 @SpringBootTest
 class DevopsbuddyApplicationTests {
 
+	
+	@Autowired
+	private I18NService i18NService;
+	
 	@Test
-	void contextLoads() {
+	public void testMessageByLocaleService() throws Exception{
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18NService.getMessage(messageId);
+		Assert.hasText(actual, "falhou em encontrar messageId");
 	}
 
 }
